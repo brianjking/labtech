214c4d27e1d0c4467de669a0c1003ea70b9e80f0
Michał Frąckiewicz <michal@lvlup.pro>
2015-10-05 23:50:24 +0200

    First demo

--- src/views/404/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    template: require('./template.html'),
-    replace: true,
-    ready: function () {
-        this.$root.$set('title', '404');
-        jQuery('.button-collapse').sideNav('hide');
-    }
-}
